cffe1375890cf2b3bb5bbf678eb6cca854713ee2
Oliver Buschjost <Oliver.Buschjost@feelgood.de>
2014-06-02 16:00:57 +0200

    Package new versions

--- support/set-env.sh
+++ support/set-env.sh
@@ -3,14 +3,14 @@
 export S3_BUCKET="feelgood-heroku-buildpack"
 
 export LIBMCRYPT_VERSION="2.5.8"
-export PHP_VERSION="5.5.12"
+export PHP_VERSION="5.5.13"
 export APC_VERSION="3.1.13"
 export PHPREDIS_VERSION="2.2.5"
 export LIBMEMCACHED_VERSION="1.0.7"
-export MONGOCLIENT_VERSION="1.5.1"
+export MONGOCLIENT_VERSION="1.5.2"
 export TWIG_VERSION="1.15.1"
 export MEMCACHED_VERSION="2.0.1"
-export NEWRELIC_VERSION="4.7.5.43"
+export NEWRELIC_VERSION="4.9.0.54"
 export XCACHE_VERSION="3.1.0"
 export IMAGICK_VERSION="3.1.0RC2"
 export NODE_VERSION="0.10.26"
@@ -18,8 +18,8 @@ export LIBICU_VERSION="50.1.2"
 export LIBPNG_VERSION="1.6.3"
 export IMAGE_MAGICK_VERSION="6.8.6-9"
 export PCRE_VERSION="8.33"
-export NGINX_VERSION="1.7.0"
-export NPM_VERSION="1.4.4"
+export NGINX_VERSION="1.7.1"
+export NPM_VERSION="1.4.9"
 
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
 export EC2_CERT=~/.ec2/cert.pem

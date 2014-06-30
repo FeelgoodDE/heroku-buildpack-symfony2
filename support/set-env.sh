3042f70bd5547af4099d8af0c74113597e063e80
Oliver Buschjost <Oliver.Buschjost@feelgood.de>
2014-06-30 15:14:16 +0200

    Upgrade NGIUNX, PHP and MongoClient driver

--- support/set-env.sh
+++ support/set-env.sh
@@ -3,11 +3,11 @@
 export S3_BUCKET="feelgood-heroku-buildpack"
 
 export LIBMCRYPT_VERSION="2.5.8"
-export PHP_VERSION="5.5.13"
+export PHP_VERSION="5.5.14"
 export APC_VERSION="3.1.13"
 export PHPREDIS_VERSION="2.2.5"
 export LIBMEMCACHED_VERSION="1.0.7"
-export MONGOCLIENT_VERSION="1.5.2"
+export MONGOCLIENT_VERSION="1.5.4"
 export TWIG_VERSION="1.15.1"
 export MEMCACHED_VERSION="2.0.1"
 export NEWRELIC_VERSION="4.9.0.54"
@@ -18,7 +18,7 @@ export LIBICU_VERSION="50.1.2"
 export LIBPNG_VERSION="1.6.3"
 export IMAGE_MAGICK_VERSION="6.8.6-9"
 export PCRE_VERSION="8.33"
-export NGINX_VERSION="1.7.1"
+export NGINX_VERSION="1.7.2"
 export NPM_VERSION="1.4.9"
 
 export EC2_PRIVATE_KEY=~/.ec2/pk.pem
